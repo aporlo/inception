7caf031d28e2426768b3083abe282b3c8b0ae4a3
aporlo <aporloporne@gmail.com>
2024-04-02 02:15:45 +0700

    fix forbidden

--- srcs/requirements/mariadb/tools/mariadb.sh
+++ srcs/requirements/mariadb/tools/mariadb.sh
@@ -1,4 +1,4 @@
-!/bin/bash
+#!/bin/bash
 
 service mariadb start
 
@@ -9,7 +9,11 @@ then
 
     # Init Databases
     mariadb -e "CREATE DATABASE IF NOT EXISTS \`${DB_NAME}\`;"
-    mariadb -e "CREATE USER IF NOT EXISTS \`${DB_USER}\`@'localhost' IDENTIFIED BY \`${DB_PASSWORD}\`;"
-    mariadb -e "GRANT ALL PRIVILEGES ON \`${DB_NAME}\`.* TO \`${DB_USER}\`@'localhost' IDENTIFIED BY \`${DB_PASSWORD}\`;"
+    mariadb -e "CREATE USER IF NOT EXISTS \`${DB_USER}\`@'%' IDENTIFIED BY '${DB_PASSWORD}';"
+    mariadb -e "GRANT ALL PRIVILEGES ON \`${DB_NAME}\`.* TO \`${DB_USER}\`@'%' IDENTIFIED BY '${DB_PASSWORD}';"
     mariadb -e "FLUSH PRIVILEGES;"
 fi
+
+service mariadb stop
+
+exec "$@"
--- srcs/requirements/wordpress/tools/wordpress.sh
+++ srcs/requirements/wordpress/tools/wordpress.sh
@@ -1,4 +1,3 @@
-
 #!/bin/bash
 
 if [ ! -e "/var/www/html/wordpress/wp-config.php" ]
@@ -7,13 +6,13 @@ then
 	cp wp-config-sample.php wp-config.php
 
 	# Init Wordpress Databases
-	sed -i "s/database_name_here/$DB_NAME/g" wp-config.php
-	sed -i "s/username_here/$DB_USER/g" wp-config.php
-	sed -i "s/password_here/$DB_PASSWORD/g" wp-config.php
-	sed -i "s/localhost/$DB_HOSTNAME/g" wp-config.php
+	sed -i "s/database_name_here/${DB_NAME}/g" wp-config.php
+	sed -i "s/username_here/${DB_USER}/g" wp-config.php
+	sed -i "s/password_here/${DB_PASSWORD}/g" wp-config.php
+	sed -i "s/localhost/${DB_HOSTNAME}/g" wp-config.php
 
 	#Certain for MariaDB has had time to launch correctly.
-	# sleep 10
+	sleep 10
 	wp core install --url='localhost' \
 		--title=${WP_TITLE}\
 		--admin_user=${ADMIN_USER}\
